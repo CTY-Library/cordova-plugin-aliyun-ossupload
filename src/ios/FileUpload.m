150dc501405af3490ca45b933f0d784c8cd04c02
noah-lou-chen <chenlounai@zzltop.com>
2019-11-14 17:37:00 +0800

    update

--- src/ios/FileUpload.m
+++ src/ios/FileUpload.m
@@ -2,13 +2,18 @@
 
 #import <Cordova/CDV.h>
 #import <AliyunOSSiOS/OSSService.h>
- 
+#import "OSSManager.h"
+#import "OSSWrapper.h"
 
 @interface FileUpload : CDVPlugin {
     NSString *uploadFilePath;//上传文件的路径
     NSString *objectKey;     //对象名字
     OSSWrapper *oss;
-    NSString *mkey; 
+    NSString *mkey;
+    NSString *endPoint;
+    
+    //@property (nonatomic, strong) OSSWrapper *oss;
+
 }
 
 - (void)putObject:(CDVInvokedUrlCommand*)command;
@@ -18,7 +23,7 @@
 
 - (void)pluginInitialize {
     CDVViewController *viewController = (CDVViewController *)self.viewController;
-    self.mkey = [viewController.settings objectForKey:@"SECRET_KEY"];//获取插件的SECRET_KEY
+    mkey = [viewController.settings objectForKey:@"SECRET_KEY"];//获取插件的SECRET_KEY
 }
 
 
@@ -27,15 +32,17 @@
 { 
     CDVPluginResult* pluginResult = nil;
     NSString* str_data = [command.arguments objectAtIndex:0]; 
-    NSString* endPoint =  [command.arguments objectAtIndex:1];
+    endPoint =  [command.arguments objectAtIndex:1];
     NSString* bucket =    [command.arguments objectAtIndex:2];
-    self.objectKey =    [command.arguments objectAtIndex:3];
-    self.uploadFilePath = [command.arguments objectAtIndex:4];
+    objectKey =  [command.arguments objectAtIndex:3];
+    uploadFilePath = [command.arguments objectAtIndex:4];
     [self initDefaultClient:str_data]; 
-    [self.oss asyncPutImage:self.objectKey localFilePath:self.uploadFilePath success:^(NSString *result) {
-      pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"success"];
+    [oss asyncPutImage:objectKey localFilePath:uploadFilePath success:^(NSString *result) {
+      //pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"success"];
+        OSSLogDebug(@"上传文件 OK");
     } failure:^(NSError *error) { 
-      pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+      //pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription];
+        OSSLogDebug(@"上传文件 error");
     }];   
 
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
@@ -44,17 +51,16 @@
  - (void) initDefaultClient:(NSString*)str_data {
     
      // 针对只有一个region下bucket的数据上传下载操作时,可以将client实例给App单例持有。
-    NSString* _oss_STSTOKEN_URL = [command.arguments objectAtIndex:0];
-    id<OSSCredentialProvider> credentialProvider = [[OSSAuthCredentialProvider alloc] initWithAuthServerUrl:str_data,mkey:self.mkey];
+    id<OSSCredentialProvider> credentialProvider = [[OSSAuthCredentialProvider alloc] initWithAuthServerUrl:str_data  mkey:mkey];
     OSSClientConfiguration *cfg = [[OSSClientConfiguration alloc] init];
     cfg.maxRetryCount = 3;
     cfg.timeoutIntervalForRequest = 15;
     cfg.isHttpdnsEnable = NO;
     cfg.crc64Verifiable = YES;
     
-    OSSClient *defaultClient = [[OSSClient alloc] initWithEndpoint:OSS_ENDPOINT credentialProvider:credentialProvider clientConfiguration:cfg];
+    OSSClient *defaultClient = [[OSSClient alloc] initWithEndpoint:endPoint credentialProvider:credentialProvider clientConfiguration:cfg];
     [OSSManager sharedManager].defaultClient = defaultClient;
- 
+  
  }
   
 
---
+++ src/ios/wrapper/OSSWrapper.m
@@ -0,0 +1,66 @@
+//
+//  OSSWrapper.m
+//  AliyunOSSSDK-iOS-Example
+//
+//  Created by huaixu on 2018/10/23.
+//  Copyright © 2018 aliyun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "OSSWrapper.h"
+#import "OSSManager.h"
+#import "OSSTestMacros.h"
+
+@interface OSSWrapper ()
+
+@property (nonatomic, strong) OSSPutObjectRequest *normalUploadRequest;
+
+@property (nonatomic, strong) OSSGetObjectRequest *normalDloadRequest;
+
+@end
+
+// 字体，默认文泉驿正黑
+NSString * const font = @"d3F5LXplbmhlaQ==";
+
+@implementation OSSWrapper
+
+ 
+- (void)asyncPutImage:(NSString *)objectKey localFilePath:(NSString *)filePath success:(void (^)(NSString *_Nullable))success failure:(void (^)(NSError * _Nonnull))failure {
+    if (![objectKey oss_isNotEmpty]) {
+        NSError *error = [NSError errorWithDomain:NSInvalidArgumentException code:0 userInfo:@{NSLocalizedDescriptionKey: @"objectKey should not be nil"}];
+        failure(error);
+        return;
+    }
+    
+    _normalUploadRequest = [OSSPutObjectRequest new];
+    _normalUploadRequest.bucketName = OSS_BUCKET_PRIVATE;
+    _normalUploadRequest.objectKey = objectKey;
+    _normalUploadRequest.uploadingFileURL = [NSURL fileURLWithPath:filePath];
+    _normalUploadRequest.isAuthenticationRequired = YES;
+    _normalUploadRequest.uploadProgress = ^(int64_t bytesSent, int64_t totalByteSent, int64_t totalBytesExpectedToSend) {
+        float progress = 1.f * totalByteSent / totalBytesExpectedToSend;
+        OSSLogDebug(@"上传文件进度: %f", progress);
+    };
+    _normalUploadRequest.callbackParam = @{
+                                           @"callbackUrl": OSS_CALLBACK_URL,
+                                           // callbackBody可自定义传入的信息
+                                           @"callbackBody": @"filename=${object}"
+                                           };
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        OSSTask * task = [[OSSManager sharedManager].defaultClient putObject:_normalUploadRequest];
+        [task continueWithBlock:^id(OSSTask *task) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (task.error) {
+                    failure(task.error);
+                } else {
+                    success(nil);
+                }
+            });
+            
+            return nil;
+        }];
+    });
+}
+ 
+ 
+@end
